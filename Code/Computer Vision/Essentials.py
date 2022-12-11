02811c35036830c3570be51f6eccf70da0b7e574
panagiota <moraiti.giota2001@gmail.com>
2022-12-11 03:22:21 +0200

    cv2 essentials

--- Code/Computer Vision/Essentials.py
+++ Code/Computer Vision/Essentials.py
@@ -1,82 +1,118 @@
 import cv2           #OpenCV
 import numpy as np   #Numpy για αριθμητικές πράξεις με πίνακες
 
-# filename = 'lena.png'
-# # filename = 'sudoku.jpg'
-# # filename = 'tiger.png'
-# # filename = 'j.png'
-#
-# img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-# print(img.shape)  # Τυπώνει τις διαστάσεις της εικόνας
-#
-# cv2.namedWindow('original')  # Δημιουργία παραθύρου με όνομα "main"
-# cv2.imshow('original', img)  # Προβολή εικόνας στο παράθυρο main
-# cv2.waitKey(0)           # Διατηρεί ανοιχτό το πρόγραμμα μέχρι να πατηθεί κάτι
-#
-# cv2.namedWindow('blur')
-# img2 = cv2.blur(img, (5, 5))
-# cv2.imshow('blur', img2)
-# cv2.waitKey(0)
-#
-# cv2.namedWindow('GaussianBlur')
-# # img2 = cv2.GaussianBlur(img, (5,5), 0)
-# # img2 = cv2.GaussianBlur(img, (5,5), 3)
-# img2 = cv2.GaussianBlur(img, (5,5), 5)
-# cv2.imshow('GaussianBlur', img2)
-# cv2.waitKey(0)
-#
-# cv2.namedWindow('medianBlur')
-# img2 = cv2.medianBlur(img,5)
-# cv2.imshow('medianBlur', img2)
-# cv2.waitKey(0)
-#
-# cv2.namedWindow('Sobel1')
-# img2 = cv2.Sobel(img, cv2.CV_8UC1, 0, 1)
-# cv2.imshow('Sobel1', img2)
-# cv2.waitKey(0)
-#
-# cv2.namedWindow('Sobel2')
-# img2 = cv2.Sobel(img, cv2.CV_8UC1, 1, 0)
-# cv2.imshow('Sobel2', img2)
-# cv2.waitKey(0)
-#
-# cv2.namedWindow('Sobel')
-# img2 = cv2.Sobel(img, cv2.CV_8UC1, 1, 1)
-# cv2.imshow('Sobel', img2)
-# cv2.waitKey(0)
-#
-# cv2.namedWindow('Laplacian')
-# img2 = cv2.Laplacian(img, cv2.CV_8UC1)
-# cv2.imshow('Laplacian', img2)
-# cv2.waitKey(0)
-#
-# cv2.namedWindow('Canny')
-# img2 = cv2.Canny(img, 100, 100)
-# cv2.imshow('Canny', img2)
-# cv2.waitKey(0)
-#
-# # filter = 1/9*np.ones((3,3))
-# # Or
-# filter = 1/9*np.array([[1,1,1], [1,1,1], [1,1,1]])
-#
-# cv2.namedWindow('filter2D')
-# img2 = cv2.filter2D(img, cv2.CV_8UC1, filter)
-# cv2.imshow('filter2D', img2)
-# cv2.waitKey(0)
-#
-# # ----------------------------------------------------------------------------------------
-#
-# filename = 'tiger.png'
-# img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-# cv2.namedWindow('median')
-# img2 = cv2.medianBlur(img,3)
-# cv2.imshow('median', img2)
-# cv2.waitKey(0)
-#
-# # Αποθήκευση εικόνας σε αρχείο
-# cv2.imwrite('tiger2.png',img2)
+filename = 'lena.png'
+# filename = 'sudoku.jpg'
+
+img_color = cv2.imread(filename, cv2.IMREAD_COLOR)
+print(img_color.shape)  # Τυπώνει τις διαστάσεις της εικόνας
+
+cv2.namedWindow('original_color')  # Δημιουργία παραθύρου με όνομα "main"
+cv2.imshow('original_color', img_color)  # Προβολή εικόνας στο παράθυρο main
+cv2.waitKey(0)           # Διατηρεί ανοιχτό το πρόγραμμα μέχρι να πατηθεί κάτι
+
+img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+# img = cv2.imread(filename, cv2.IMREAD_COLOR)
+print(img.shape)  # Τυπώνει τις διαστάσεις της εικόνας
+
+cv2.namedWindow('original')  # Δημιουργία παραθύρου με όνομα "main"
+cv2.imshow('original', img)  # Προβολή εικόνας στο παράθυρο main
+cv2.waitKey(0)           # Διατηρεί ανοιχτό το πρόγραμμα μέχρι να πατηθεί κάτι
+
+cv2.namedWindow('blur')
+img2 = cv2.blur(img, (5, 5))
+cv2.imshow('blur', img2)
+cv2.waitKey(0)
+
+cv2.namedWindow('GaussianBlur')
+# img2 = cv2.GaussianBlur(img, (5,5), 0)
+# img2 = cv2.GaussianBlur(img, (5,5), 3)
+img2 = cv2.GaussianBlur(img, (5,5), 5)
+cv2.imshow('GaussianBlur', img2)
+cv2.waitKey(0)
+
+cv2.namedWindow('medianBlur')
+img2 = cv2.medianBlur(img,5)
+cv2.imshow('medianBlur', img2)
+cv2.waitKey(0)
+
+cv2.namedWindow('Sobel1')
+img2 = cv2.Sobel(img, cv2.CV_8UC1, 0, 1)
+cv2.imshow('Sobel1', img2)
+cv2.waitKey(0)
+
+cv2.namedWindow('Sobel2')
+img2 = cv2.Sobel(img, cv2.CV_8UC1, 1, 0)
+cv2.imshow('Sobel2', img2)
+cv2.waitKey(0)
+
+cv2.namedWindow('Sobel')
+img2 = cv2.Sobel(img, cv2.CV_8UC1, 1, 1)
+cv2.imshow('Sobel', img2)
+cv2.waitKey(0)
+
+cv2.namedWindow('Laplacian')
+img2 = cv2.Laplacian(img, cv2.CV_8UC1)
+cv2.imshow('Laplacian', img2)
+cv2.waitKey(0)
+
+cv2.namedWindow('Canny')
+img2 = cv2.Canny(img, 100, 100)
+cv2.imshow('Canny', img2)
+cv2.waitKey(0)
+
+# filter = 1/9*np.ones((3,3))
+# Or
+filter = 1/9*np.array([[1,1,1], [1,1,1], [1,1,1]])
+
+cv2.namedWindow('filter2D')
+img2 = cv2.filter2D(img, cv2.CV_8UC1, filter)
+cv2.imshow('filter2D', img2)
+cv2.waitKey(0)
 
 # ----------------------------------------------------------------------------------------
+
+filename = 'tiger.png'
+img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+
+cv2.namedWindow('originalt')  # Δημιουργία παραθύρου με όνομα "main"
+cv2.imshow('originalt', img)  # Προβολή εικόνας στο παράθυρο main
+cv2.waitKey(0)           # Διατηρεί ανοιχτό το πρόγραμμα μέχρι να πατηθεί κάτι
+
+cv2.namedWindow('median')
+img2 = cv2.medianBlur(img,3)
+cv2.imshow('median', img2)
+cv2.waitKey(0)
+
+# Αποθήκευση εικόνας σε αρχείο
+cv2.imwrite('tiger2.png',img2)
+
+# ----------------------------------------------------------------------------------------
+
+filename = 'sudoku.jpg'
+img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+
+cv2.namedWindow('originals')  # Δημιουργία παραθύρου με όνομα "main"
+cv2.imshow('originals', img)  # Προβολή εικόνας στο παράθυρο main
+cv2.waitKey(0)           # Διατηρεί ανοιχτό το πρόγραμμα μέχρι να πατηθεί κάτι
+
+cv2.namedWindow('Sobel1s')
+img2 = cv2.Sobel(img, cv2.CV_8UC1, 0, 1)
+cv2.imshow('Sobel1s', img2)
+cv2.waitKey(0)
+
+cv2.namedWindow('Sobel2s')
+img2 = cv2.Sobel(img, cv2.CV_8UC1, 1, 0)
+cv2.imshow('Sobel2s', img2)
+cv2.waitKey(0)
+
+cv2.namedWindow('Sobels')
+img2 = cv2.Sobel(img, cv2.CV_8UC1, 1, 1)
+cv2.imshow('Sobels', img2)
+cv2.waitKey(0)
+
+# ----------------------------------------------------------------------------------------
+
 filename = 'j.png'
 
 img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
